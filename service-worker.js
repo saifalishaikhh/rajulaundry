6ea273b01f0877b5d4880812548b23045ca8bad0
saifalishaikhh <120360512+saifalishaikhh@users.noreply.github.com>
2025-12-15 02:49:15 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,25 @@
-const CACHE_NAME = "raju-laundry-v1";
+const CACHE_NAME = "raju-laundry-v2";
 
 const FILES_TO_CACHE = [
-    "/rajulaundry/",
-    "/rajulaundry/index.html",
-    "/rajulaundry/css/style.css",
-    "/rajulaundry/js/pwa.js",
-    "/rajulaundry/manifest.json",
-    "/rajulaundry/images/logo-192.png",
-    "/rajulaundry/images/logo-512.png",
-    "/rajulaundry/images/qr-code.png"
+  "/rajulaundry/",
+  "/rajulaundry/index.html",
+  "/rajulaundry/css/style.css",
+  "/rajulaundry/js/pwa.js",
+  "/rajulaundry/manifest.json",
+  "/rajulaundry/images/logo-192.png",
+  "/rajulaundry/images/logo-512.png"
 ];
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log("Caching files");
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
